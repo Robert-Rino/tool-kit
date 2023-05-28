ee0cdb119dc287c8015942d97e9f57a405d1cc4e
Robert-Rino <a901002666@gmail.com>
2023-05-28 23:32:11 +0800

    WIP matrix

--- uptrace/app.py
+++ uptrace/app.py
@@ -28,6 +28,8 @@ from flask import Flask
 resource = Resource(
     attributes={"service.name": "myservice", "service.version": "1.0.0"}
 )
+
+# Trace
 tracer_provider = TracerProvider(
     resource=resource,
 )
@@ -49,24 +51,37 @@ span_processor = BatchSpanProcessor(
 tracer_provider.add_span_processor(span_processor)
 tracer = trace.get_tracer("app_or_package_name", "1.0.0")
 
-# ==
-# tracer = trace.get_tracer("app_or_package_name", "1.0.0")
 
-# tracer = trace.get_tracer("diceroller.tracer")
-# # Acquire a meter.
-# meter = metrics.get_meter("diceroller.meter")
+# Metrix
+temporality_delta = {
+    sdkmetrics.Counter: AggregationTemporality.DELTA,
+    sdkmetrics.UpDownCounter: AggregationTemporality.DELTA,
+    sdkmetrics.Histogram: AggregationTemporality.DELTA,
+    sdkmetrics.ObservableCounter: AggregationTemporality.DELTA,
+    sdkmetrics.ObservableUpDownCounter: AggregationTemporality.DELTA,
+    sdkmetrics.ObservableGauge: AggregationTemporality.DELTA,
+}
 
-# # Now create a counter instrument to make measurements with
-# roll_counter = meter.create_counter(
-#     "roll_counter",
-#     description="The number of rolls by roll value",
-# )
+metric_exporter = OTLPMetricExporter(
+    endpoint="http://otelcol:4317",
+    # headers=(("uptrace-dsn", dsn),),
+    timeout=5,
+    compression=grpc.Compression.Gzip,
+    preferred_temporality=temporality_delta,
+)
+reader = PeriodicExportingMetricReader(metric_exporter)
+provider = MeterProvider(metric_readers=[reader], resource=resource)
+metrics.set_meter_provider(provider)
+meter = metrics.get_meter("github.com/uptrace/uptrace-python", "1.0.0")
+counter = meter.create_counter("some.prefix.counter", description="TODO")
+
 
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 
 @app.route("/rolldice")
 def roll_dice():
+    counter.add(1)
     return str(do_roll())
 
 def do_roll():
