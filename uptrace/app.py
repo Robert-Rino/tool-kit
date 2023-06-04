dd6f88098e6d2f61dbe64139120a03a382b6c6bd
Robert-Rino <a901002666@gmail.com>
2023-06-04 15:49:11 +0800

    WIP

--- uptrace/app.py
+++ uptrace/app.py
@@ -79,9 +79,16 @@ counter = meter.create_counter("some.prefix.counter", description="TODO")
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 
+@app.route("/rolldice2")
+def toll_dice2():
+    return str(randint(1, 6))
+
 @app.route("/rolldice")
 def roll_dice():
     counter.add(1)
+    rolled = randint(1, 6)
+    time.sleep(0.1 * rolled)
+    return str(rolled)
     return str(do_roll())
 
 def do_roll():
@@ -93,83 +100,3 @@ def do_roll():
         # This adds 1 to the counter for the given roll value
         # roll_counter.add(1, {"roll.value": res})
         return res
-
-
-# import os
-# import sentry_sdk
-# import sentry_sdk.integrations.flask
-
-# from flask import Flask, request, current_app
-# from random import randint
-
-
-# from opentelemetry import trace
-# from opentelemetry import metrics
-# from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-# from opentelemetry.sdk.metrics import MeterProvider
-# from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-# from opentelemetry.instrumentation.flask import FlaskInstrumentor
-
-
-# from opentelemetry import trace
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-
-
-# # # Service name is required for most backends
-# # resource = Resource(attributes={
-# #     SERVICE_NAME: "nino-flask"
-# # })
-
-
-# # # Matrix
-# # reader = PeriodicExportingMetricReader(
-# #     OTLPMetricExporter(endpoint="http://otelcol:4317")
-# # )
-# # meter_provider = MeterProvider(resource=resource, metric_readers=[reader])
-# # metrics.set_meter_provider(meter_provider)
-
-# # tracer = trace.get_tracer("diceroller.tracer")
-# # # Acquire a meter.
-# # meter = metrics.get_meter("diceroller.meter")
-
-# # # Now create a counter instrument to make measurements with
-# # roll_counter = meter.create_counter(
-# #     "roll_counter",
-# #     description="The number of rolls by roll value",
-# # )
-
-
-# # # Trace
-# # trace_provider = TracerProvider(resource=resource)
-# # processor = BatchSpanProcessor(
-# #     ConsoleSpanExporter()
-# # )
-# # trace_provider.add_span_processor(processor)
-# # trace.set_tracer_provider(trace_provider)
-
-# tracer = trace.get_tracer("diceroller.tracer")
-# # Acquire a meter.
-# meter = metrics.get_meter("diceroller.meter")
-
-# # Now create a counter instrument to make measurements with
-# roll_counter = meter.create_counter(
-#     "roll_counter",
-#     description="The number of rolls by roll value",
-# )
-
-
-# app = Flask(__name__)
-# # FlaskInstrumentor().instrument_app(app)
-
-# @app.route("/")
-# def hello_world():
-#     with tracer.start_as_current_span("do_roll") as rollspan:
-#         res = randint(1, 6)
-#         rollspan.set_attribute("roll.value", res)
-#         # This adds 1 to the counter for the given roll value
-#         roll_counter.add(1, {"roll.value": res})
-#         return str(res)
-
-
