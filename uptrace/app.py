438787a81c511b59a0d3bae354644bec95dd487e
Robert-Rino <a901002666@gmail.com>
2023-06-22 18:17:24 +0800

    Add mongo otel

--- uptrace/app.py
+++ uptrace/app.py
@@ -1,7 +1,9 @@
 import os
 import time
-
 import grpc
+
+
+from opentelemetry.instrumentation.pymongo import PymongoInstrumentor
 
 from opentelemetry import trace, metrics
 from opentelemetry.sdk import metrics as sdkmetrics
@@ -23,7 +25,8 @@ from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 from random import randint
-from flask import Flask
+from flask import Flask, request, Response, make_response
+from flask_mongoengine import MongoEngine
 
 resource = Resource(
     attributes={"service.name": "myservice", "service.version": "1.0.0"}
@@ -77,13 +80,54 @@ counter = meter.create_counter("some.prefix.counter", description="TODO")
 
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://mongo/dev'
+}
+# NOTE: Need to call instrument before mongoengine initialize.
+PymongoInstrumentor().instrument()
+db = MongoEngine(app)
 FlaskInstrumentor().instrument_app(app)
 
-@app.route("/rolldice2")
+
+class User(db.Document):
+    username = db.StringField(required=True)
+
+
+@app.route('/user', methods={'POST'}, endpoint='create_user')
+def create_user():
+    payload = request.get_json()
+
+    if not (username := payload.get('username')):
+        return Response(status=400)
+    
+    if user := User.objects.create(username=username):
+        response = make_response({
+            'id': str(user.id),
+            'username': user.username
+        })
+        response.status = 201
+        return response
+    
+
+@app.route('/user/<string:user_id>', methods={'GET'}, endpoint='get_user_by_id')
+def get_user_by_id(user_id: str):
+    
+    if not (user := User.objects.filter(id=user_id).first()):
+        return Response(status=404)
+    
+
+    response = make_response({
+        'id': str(user.id),
+        'username': user.username
+    })
+    return response
+
+
+@app.route("/roll")
 def toll_dice2():
     return str(randint(1, 6))
 
-@app.route("/rolldice")
+@app.route("/roll-long")
 def roll_dice():
     counter.add(1)
     rolled = randint(1, 6)
